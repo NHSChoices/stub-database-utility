6426c0c270055d1da97826acebe2b98f74050187
Neil McLaughlin <neil.mclaughlin4@nhs.net>
2015-01-14 14:24:20 +0000

    Refactored by extract to method for legibility

--- NHSChoices.StubDatabaseUtility.Tests.Integration/StubDatabaseTests.cs
+++ NHSChoices.StubDatabaseUtility.Tests.Integration/StubDatabaseTests.cs
@@ -26,14 +26,8 @@
       //Arrange
       var sourceServerConnection = GetServerConnection("Source");
       var destinationServerConnection = GetServerConnection("Target");
-      var destinationServer = new Server(destinationServerConnection);
-
-      if (destinationServer.Databases[destinationServerConnection.DatabaseName] != null)
-      {
-        destinationServer.Databases[destinationServerConnection.DatabaseName].Drop();        
-      }
-
-      var stubDatabaseUtility = new StubDatabaseUtility(destinationServerConnection);
+      DropDatabase(destinationServerConnection);
+      var stubDatabaseUtility = GetStubDatabaseUtility(destinationServerConnection);
 
       //Act
       stubDatabaseUtility.CopySchemaFromDatabase(sourceServerConnection);
@@ -47,6 +41,35 @@
     public void DatabaseTableClear()
     {
       //Arrange
+      var tableList = GetTableList();
+      var serverConnection = GetServerConnection("Target");
+      var stubDatabaseUtility = GetStubDatabaseUtility(serverConnection);
+
+      //Act
+      stubDatabaseUtility.ClearDatabaseTables(tableList);
+
+      //Assert
+      Assert.That(new Server(serverConnection).Databases[serverConnection.DatabaseName].Tables[0].RowCount, Is.EqualTo(0));
+
+    }
+
+    private static void DropDatabase(ServerConnection destinationServerConnection)
+    {
+      var destinationServer = new Server(destinationServerConnection);
+
+      if (destinationServer.Databases[destinationServerConnection.DatabaseName] != null)
+      {
+        destinationServer.Databases[destinationServerConnection.DatabaseName].Drop();
+      }
+    }
+
+    private static StubDatabaseUtility GetStubDatabaseUtility(ServerConnection serverConnection)
+    {
+      return new StubDatabaseUtility(serverConnection);
+    }
+
+    private static string[] GetTableList()
+    {
       var assembly = Assembly.GetExecutingAssembly();
       const string resourceName = "NHSChoices.StubDatabaseUtility.Tests.Integration.TestResources.TableNames.txt";
 
@@ -55,18 +78,8 @@
       {
         result = reader.ReadToEnd();
       }
-      var tableList = result.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-
-      var serverConnection = GetServerConnection("Target");
-      var stubDatabaseUtility = new StubDatabaseUtility(serverConnection);
-
-      //Act
-
-      stubDatabaseUtility.ClearDatabaseTables(tableList);
-
-      //Assert
-      Assert.That(new Server(serverConnection).Databases[serverConnection.DatabaseName].Tables[0].RowCount, Is.EqualTo(0));
-
+      var tableList = result.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+      return tableList;
     }
 
     private static ServerConnection GetServerConnection(string connectionStringName)
